d9ccf5e21422fd185ad5d8f10c0d528bf16540b6
Kross55 <iyakushenko84@gmail.com>
2023-03-28 23:03:28 +0300

    hw34Form

--- homework/hw34/index.js
+++ homework/hw34/index.js
@@ -2,7 +2,8 @@ const form = document.getElementById('registration-form');
 const table = document.getElementById('registration-data');
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault(); 
+
+  e.preventDefault(); //prevent submit
 
   const formData = new FormData(form); 
   const tableRow = document.createElement('tr'); 
@@ -14,6 +15,7 @@ form.addEventListener('submit', (e) => {
   });
 
   table.querySelector('tbody').appendChild(tableRow);
+
   form.reset();
   form.style.display = 'none';
   table.style.display = 'table';
