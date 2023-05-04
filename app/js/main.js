5af1d17a722c3b2110513fc7a52ae8413a279c96
Kross55 <iyakushenko84@gmail.com>
2023-05-04 16:04:03 +0300

    gulp_base&eslint

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
-$(function () {
-    $('body').css('background', 'blue')
-})
+$(() => {
+  $("body").css("background", "blue");
+});
+
+const adventurer = {
+  name: "Alice",
+  cat: {
+    name: "Dinah",
+  },
+};
+const dogName = adventurer.dog?.name;
+// eslint-disable-next-line no-console
+console.log(dogName);
+// Expected output: undefined
+// eslint-disable-next-line no-console
+console.log(adventurer.someNonExistentMethod?.());
+// Expected output: undefined
